e6fe8a3f0c85d83e3b9921689cb99af70fb3d5bf
Tania Mishakova <tmishakova123@gmail.com>
2021-05-08 16:50:27 +0200

    added remove buttons, toggle buttons

---
+++ js/script.js
@@ -0,0 +1,76 @@
+{
+  const tasks = [];
+
+
+  const addNewTask = () => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const removeTask = (taskIndeks) => {
+    tasks.splice(taskIndeks, 1);
+    render();
+  }
+
+  const toggleTaskDone = (taskIndeks) => {
+    tasks[taskIndeks].done = !tasks[taskIndeks].done;
+    render();
+  }
+
+  const render = () => {
+    let htmlString = "";
+    for (const task of tasks) {
+      htmlString += `
+            <li class="list__item${task.done ? "list__item--done" : "list__item--delete"}">
+            <button class="list__button list__button--done js-doneButton">✔️</button>
+           ${task.content}
+              <button class="list__button list__button--delete js-remove">🗑️</button> 
+            </li>
+            `;
+    }
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
+
+  const toggleDoneButtons = document.querySelectorAll(".js-done")
+
+  toggleDoneButtons.forEach((toggleDoneButton, index) => {
+    toggleDoneButton.addEventListener("click", () => {
+      toggleTaskDone(index);
+    });
+  });
+};
+
+
+const removeButtons = document.querySelectorAll(".js-remove");
+
+removeButtons.forEach((removeButton, index) => {
+  removeButton.addEventListener("click", () => {
+    removeTask(index);
+  });
+});
+};
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+  if (newTaskContent === "") {
+    return;
+  }
+  addNewTask(newTaskContent);
+};
+
+const init = () => {
+  render();
+
+  const form = document.querySelector(".js-form");
+
+  form.addEventListener("submit", onFormSubmit);
+};
+
+init();
+}
