71d5a1ea4dd8261f091feb5c4638c8b689bd6e6c
Tania Mishakova <tmishakova123@gmail.com>
2021-05-08 16:58:34 +0200

    binding events moved to separate function

--- js/script.js
+++ js/script.js
@@ -20,6 +20,25 @@
     render();
   }
 
+  const bindEvents = () => {
+    const toggleDoneButtons = document.querySelectorAll(".js-done")
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  
+  
+  const removeButtons = document.querySelectorAll(".js-remove");
+  
+  removeButtons.forEach((removeButton, index) => {
+    removeButton.addEventListener("click", () => {
+      removeTask(index);
+    });
+  });
+  }
+
   const render = () => {
     let htmlString = "";
     for (const task of tasks) {
@@ -32,26 +51,9 @@
             `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents ();
   };
-
-  const toggleDoneButtons = document.querySelectorAll(".js-done")
-
-  toggleDoneButtons.forEach((toggleDoneButton, index) => {
-    toggleDoneButton.addEventListener("click", () => {
-      toggleTaskDone(index);
-    });
-  });
-};
-
-
-const removeButtons = document.querySelectorAll(".js-remove");
-
-removeButtons.forEach((removeButton, index) => {
-  removeButton.addEventListener("click", () => {
-    removeTask(index);
-  });
-});
-};
 
 const onFormSubmit = (event) => {
   event.preventDefault();
